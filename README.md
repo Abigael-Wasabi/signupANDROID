af9e0951c7c6afe30c140289dd56baacf76e7dd8
Abiwasabi <abiwasabi03@gmail.com>
2024-02-25 01:23:17 +0300

    added the search bar

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Simple signup Screen.
+
 A simple signup screen with username, email, password, phone number
 Android app,applied MVVM architecture, screens reps the UI, viewModels reps controllers(business
 logic), model rep structure of the db, userRepo injects firebase
 services, navigation for navigating thru the app;frm one screen to another
 #naivasDeliveryApp
+
+![img_1.png](img_1.png)
--- app/src/main/java/com/ist/simpleloginscreen/presentation/screens/proj/CartScreen.kt
+++ app/src/main/java/com/ist/simpleloginscreen/presentation/screens/proj/CartScreen.kt
@@ -30,92 +30,99 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.ist.simpleloginscreen.presentation.MainViewModel
+import com.ist.simpleloginscreen.presentation.screens.main.BottomNavigationItem
+import com.ist.simpleloginscreen.presentation.screens.main.BottomNavigationMenu
 
-fun verticalScroll(
-    rememberScrollState:
-    ScrollState,
-    function: () -> Unit,
-) = Unit
+fun verticalScroll(rememberScrollState: ScrollState, function: () -> Unit) = Unit
 
 @Composable
 fun CartScreen(navController: NavController, vm: MainViewModel, selectedItems: List<Item>) {
-    // Calculate total amount
-    val totalAmount = selectedItems.sumOf { it.price }
+    var isSearchVisible by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
 
-    // State variable to track if phone number is filled
-    var phoneNumberFilled by remember { mutableStateOf(false) }
-
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "Back",
-            modifier = Modifier.clickable { navController.popBackStack() }
+    Column {
+        BottomNavigationMenu(
+            selectedItem = BottomNavigationItem.CART,
+            navController = navController,
+            isSearchVisible = isSearchVisible,
+            onSearchTextChanged = { text -> searchText = text },
+            onSearchIconClicked = { isSearchVisible = !isSearchVisible }
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        InputField(label = "Phone Number") { filled -> phoneNumberFilled = filled }
-        Spacer(modifier = Modifier.height(16.dp))
-//        LocationDropdown(
-//            label = "Location",
-//            options = listOf("Nairobi", "Kigali", "Kampala")
-//        )
-//        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Selected Items:")
-        Spacer(modifier = Modifier.height(8.dp))
 
-        // Display the selected items in a LazyColumnRow with scrollbar
-        LazyColumn(
-            modifier = Modifier.weight(1f),
-            contentPadding = PaddingValues(horizontal = 8.dp)
-        ) {
-            items(selectedItems) { item ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        painter = rememberAsyncImagePainter(item.imageUrl),
-                        contentDescription = null,
-                        modifier = Modifier.size(64.dp)
-                    )
-                    Column(
-                        modifier = Modifier.padding(start = 16.dp)
+        // Calculate total amount
+        val totalAmount = selectedItems.sumOf { it.price }
+
+        // State variable to track if phone number is filled
+        var phoneNumberFilled by remember { mutableStateOf(false) }
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Back",
+                modifier = Modifier.clickable { navController.popBackStack() }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            InputField(label = "Phone Number") { filled -> phoneNumberFilled = filled }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Selected Items:")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Display the selected items in a LazyColumnRow with scrollbar
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                contentPadding = PaddingValues(horizontal = 8.dp)
+            ) {
+                items(selectedItems) { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Text(text = item.name)
-                        Text(text = "$${item.price}")
-                    }
-                    Button(
-                        onClick = {
-                            vm.removeFromCart(item.name)
-//                            totalAmount-=item.price,
-                        },
-                        modifier = Modifier.padding(start = 8.dp)
-                    ) {
-                        Text(text = "Remove from cart")
+                        Image(
+                            painter = rememberAsyncImagePainter(item.imageUrl),
+                            contentDescription = null,
+                            modifier = Modifier.size(64.dp)
+                        )
+                        Column(
+                            modifier = Modifier.padding(start = 16.dp)
+                        ) {
+                            Text(text = item.name)
+                            Text(text = "$${item.price}")
+                        }
+                        Button(
+                            onClick = {
+                                vm.removeFromCart(item.name)
+//                            calculateTotalAmount() -=item.price,
+                            },
+                            modifier = Modifier.padding(start = 8.dp)
+                        ) {
+                            Text(text = "Remove from cart")
+                        }
                     }
                 }
             }
-        }
-        //scrollbar to the LazyColumn
-        verticalScroll(rememberScrollState()) {
-            // Empty composable used for scrolling
-        }
+            //scrollbar to the LazyColumn
+            verticalScroll(rememberScrollState()) {
+                // Empty composable used for scrolling
+            }
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Total Amount: \$${"%.2f".format(totalAmount)}")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { /* Pay button action */ },
-            enabled = phoneNumberFilled
-        ) {
-            Text(text = "Pay")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(
-            onClick = { /* Order button action */ },
-            enabled = phoneNumberFilled
-        ) {
-            Text(text = "Order")
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Total Amount: \$${"%.2f".format(totalAmount)}")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /* Pay button action */ },
+                enabled = phoneNumberFilled
+            ) {
+                Text(text = "Pay")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = { /* Order button action */ },
+                enabled = phoneNumberFilled
+            ) {
+                Text(text = "Order")
+            }
         }
     }
 }
@@ -128,18 +135,27 @@ data class Item(val name: String, val price: Double, val imageUrl: String)
 @Composable
 fun InputField(label: String, onFilled: (Boolean) -> Unit) {
     var text by remember { mutableStateOf(TextFieldValue()) }
-
     Column {
         Text(text = label)
         TextField(
             value = text,
             onValueChange = {
+                val isValidPhoneNumber = validatePhoneNumber(it.text)
                 text = it
-                onFilled(it.text.isNotEmpty())
+                onFilled(isValidPhoneNumber && it.text.isNotEmpty())
             },
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+//valid phone no
+fun validatePhoneNumber(phoneNumber: String): Boolean {
+    //at least 10 chars
+    if (phoneNumber.length < 10) return false
+
+    //contain only nos and the + sign
+    return phoneNumber.all { it.isDigit() || it == '+' }
 }
 
 fun calculateTotalAmount(items: List<Item>): Double {
